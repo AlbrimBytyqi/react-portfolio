f3c73476ccef85da949c0da20448303dc4ede30e
Albrim Bytyqi <bytyqialbrimaia@gmail.com>
2025-05-05 19:20:08 +0200

    About Section Framer Motion

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,6 @@
 import aboutimg from "../assets/about.jpg";
 import { ABOUT_TEXT } from "../constants";
+import { motion } from "motion/react";
 
 const About = () => {
   return (
@@ -8,16 +9,26 @@ const About = () => {
         About <span className=" text-neutral-500">Me</span>
       </h2>
       <div className=" flex flex-wrap">
-        <div className=" w-full lg:w-1/2 lg:p-8">
+        <motion.div
+          whileInView={{ x: 0, opacity: 1 }}
+          initial={{ x: -100, opacity: 0 }}
+          transition={{ duration: 0.5 }}
+          className=" w-full lg:w-1/2 lg:p-8"
+        >
           <div className=" flex items-center justify-center">
             <img className=" rounded-2xl" src={aboutimg} alt="About" />
           </div>
-        </div>
-        <div className=" w-full lg:w-1/2">
+        </motion.div>
+        <motion.div
+          whileInView={{ x: 0, opacity: 1 }}
+          initial={{ x: 100, opacity: 0 }}
+          transition={{ duration: 0.5 }}
+          className=" w-full lg:w-1/2"
+        >
           <div className=" flex justify-center lg:justify-start">
             <p className=" my-2 max-w-xl py-6">{ABOUT_TEXT}</p>
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
