05014d3fa5f8090c4bdd2b1cd98392282e8106ab
Albrim Bytyqi <bytyqialbrimaia@gmail.com>
2025-05-05 19:33:01 +0200

    Technologies Framer Motion

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -1,22 +1,63 @@
 import { RiReactjsLine } from "react-icons/ri";
 import { TbBrandNextjs } from "react-icons/tb";
 import { FaGitAlt } from "react-icons/fa";
+import { motion } from "motion/react";
+
+const iconVariants = (duration: number): { initial: any; animate: any } => ({
+  initial: { y: -10 },
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse",
+    },
+  },
+});
 
 const Technologies = () => {
   return (
     <div className=" border-b border-neutral-800 pb-24">
-      <h1 className=" my-20 text-center text-4xl">Technologies</h1>
-      <div className=" flex flex-wrap items-center justify-center gap-4">
-        <div className=" rounded-2xl border-4 border-neutral-800 p-4">
+      <motion.h1
+        whileInView={{ y: 0, opacity: 1 }}
+        initial={{ y: -100, opacity: 0 }}
+        transition={{ duration: 1.5 }}
+        className=" my-20 text-center text-4xl"
+      >
+        Technologies
+      </motion.h1>
+      <motion.div
+        whileInView={{ x: 0, opacity: 1 }}
+        initial={{ x: -100, opacity: 0 }}
+        transition={{ duration: 1.5 }}
+        className=" flex flex-wrap items-center justify-center gap-4"
+      >
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className=" rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <RiReactjsLine className=" text-7xl text-cyan-400" />
-        </div>
-        <div className=" rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className=" rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <TbBrandNextjs className=" text-7xl " />
-        </div>
-        <div className=" rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div
+          variants={iconVariants(4)}
+          initial="initial"
+          animate="animate"
+          className=" rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <FaGitAlt className=" text-7xl" style={{ color: "#F1502F" }} />
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
     </div>
   );
 };
